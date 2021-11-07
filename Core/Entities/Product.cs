e3b178be52042976dae545eaf372e4fdec98ea5a
unknown <gabriel.bohn1@gmail.com>
2021-11-07 12:17:29 -0300

    Initial Commit

---
+++ Core/Entities/Product.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        
+
+
+    // nome: string;
+    // preco: string; 
+    // peso: string; 
+    // unidade_medida: string; 
+    // categoria: string; 
+    // cod_produto: string
+    }
+}
