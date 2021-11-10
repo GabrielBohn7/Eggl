4919c024099dd28db6459599b9311c7e40e6bfc2
unknown <gabriel.bohn1@gmail.com>
2021-11-10 00:08:06 -0300

    UI

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaginationModule } from 'ngx-bootstrap/pagination'
+import { CarouselModule } from 'ngx-bootstrap/carousel'
 
 
 
@@ -8,8 +9,9 @@ import { PaginationModule } from 'ngx-bootstrap/pagination'
   declarations: [],
   imports: [
     CommonModule,
-    PaginationModule.forRoot()
+    PaginationModule.forRoot(),
+    CarouselModule.forRoot()
   ],
-  exports: [PaginationModule]
+  exports: [PaginationModule, CarouselModule]
 })
 export class SharedModule { }
